880f207c2ae964ac95a58f77a55eb5e680eb4853
Mohit Agrawal <mohit2005agrawal@gmail.com>
2026-09-10 20:30:27 +0530

    Fixed cookie settings for Vercel CORS

--- deploy/backend/src/controllers/userAuthent.js
+++ deploy/backend/src/controllers/userAuthent.js
@@ -27,7 +27,8 @@ const register = async (req,res)=>{
         role:user.role,
     }
     
-     res.cookie('token',token,{maxAge: 60*60*1000});
+     res.cookie('token',token,{ maxAge: 60*60*1000, httpOnly: true, sameSite: 'none', secure: true });
+     
      res.status(201).json({
         user:reply,
         message:"Loggin Successfully"
@@ -65,7 +66,9 @@ const login = async (req,res)=>{
         }
 
         const token =  jwt.sign({_id:user._id , emailId:emailId, role:user.role},process.env.JWT_KEY,{expiresIn: 60*60});
-        res.cookie('token',token,{maxAge: 60*60*1000});
+        
+        res.cookie('token',token,{ maxAge: 60*60*1000, httpOnly: true, sameSite: 'none', secure: true });
+        
         res.status(201).json({
             user:reply,
             message:"Loggin Successfully"
@@ -91,7 +94,8 @@ const logout = async(req,res)=>{
     //    Token add kar dung Redis ke blockList
     //    Cookies ko clear kar dena.....
 
-    res.cookie("token",null,{expires: new Date(Date.now())});
+    res.cookie("token",null,{ expires: new Date(Date.now()), httpOnly: true, sameSite: 'none', secure: true });
+    
     res.send("Logged Out Succesfully");
 
     }
@@ -114,7 +118,9 @@ const adminRegister = async(req,res)=>{
     
      const user =  await User.create(req.body);
      const token =  jwt.sign({_id:user._id , emailId:emailId, role:user.role},process.env.JWT_KEY,{expiresIn: 60*60});
-     res.cookie('token',token,{maxAge: 60*60*1000});
+     
+     res.cookie('token',token,{ maxAge: 60*60*1000, httpOnly: true, sameSite: 'none', secure: true });
+     
      res.status(201).send("User Registered Successfully");
     }
     catch(err){
